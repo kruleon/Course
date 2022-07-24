5a19756c427c423ff715efb1d68d86b27c8e7e9f
kruleon <kruleon1@gmail.com>
2022-07-24 15:12:17 +0300

    adding assignment_git.py file

---
+++ assignment_git.py
@@ -0,0 +1,9 @@
+# Git
+#
+# 1. Clone Selenium Webdriver project
+# https://github.com/SeleniumHQ/selenium
+#
+# 2. Clone Appium project
+# https://github.com/appium/appium
+#
+# 3. Create a new Python project (any) and upload it to Github
